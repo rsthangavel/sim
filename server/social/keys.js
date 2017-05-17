edc277fd109d1ad1776243979fbafae3d9c44376
Thangavel <thangavel@mails.asahitechnologies.com>
2017-05-17 18:39:20 +0530

    keys url changed

--- server/social/keys.js
+++ server/social/keys.js
@@ -3,18 +3,18 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '593292704199085', // your App ID
         'clientSecret'  : 'c5972b1842cd72368923baf0d0bf85ec', // your App Secret
-        'callbackURL'   : 'http://www.localhost:2000/api/auth/facebook/callback'
+        'callbackURL'   : 'https://angular-exp.herokuapp.com/api/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://angular-exp.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '681644594140-6u40flstt15k44901so7hvnoh43q7m5n.apps.googleusercontent.com',
         'clientSecret'  : 'OXYwfvElMWfB1It0j9A46Zh6',
-        'callbackURL'   : 'http://localhost:2000/api/auth/google/callback'
+        'callbackURL'   : 'https://angular-exp.herokuapp.com/api/auth/google/callback'
     }
 };
